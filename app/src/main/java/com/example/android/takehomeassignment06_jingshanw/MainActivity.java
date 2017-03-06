291796c8baa36b22f009e65588a812c8d1495167
jingshanW <jw3435@tc.columbia.edu>
2017-03-06 14:44:37 -0500

    final

---
+++ app/src/main/java/com/example/android/takehomeassignment06_jingshanw/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.android.takehomeassignment06_jingshanw;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static com.example.android.takehomeassignment06_jingshanw.R.id.total;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView amount;
+    private TextView taxPercentage;
+    private TextView tipPercentage;
+
+    static final int CALCULATOR_REQUEST = 1;  // The request code
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        amount= (TextView) findViewById(R.id.amount_text_view);
+        taxPercentage= (TextView) findViewById(R.id.tax_percentage);
+        tipPercentage= (TextView) findViewById(R.id.tip_percentage);
+    }
+
+    public void submit(View view)
+    {
+        Intent intent=new Intent(this,SecondActivity.class);
+        int total=Integer.parseInt(amount.getText().toString());
+        intent.putExtra("AMOUNT",total);
+        startActivity(intent);
+
+        Intent intent2=new Intent(this,SecondActivity.class);
+        int taxP=Integer.parseInt(taxPercentage.getText().toString());
+        int salesTax=total*taxP;
+        intent2.putExtra("TAX",salesTax);
+        startActivity(intent2);
+
+        Intent intent3=new Intent(this,SecondActivity.class);
+        int tipP=Integer.parseInt(tipPercentage.getText().toString());
+        int tip=total*tipP;
+        intent2.putExtra("TIPS",tip);
+        startActivity(intent3);
+
+        Intent intent4=new Intent(this,SecondActivity.class);
+        int grand=total+salesTax+tip;
+        intent2.putExtra("GRAND",grand);
+        startActivity(intent4);
+
+
+
+
+    }
+
+
+    }
+
---
+++ app/src/main/java/com/example/android/takehomeassignment06_jingshanw/SecondActivity.java
@@ -0,0 +1,43 @@
+package com.example.android.takehomeassignment06_jingshanw;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static com.example.android.takehomeassignment06_jingshanw.R.id.total;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private TextView totalAmount;
+    private TextView salesTax;
+    private TextView tip;
+    private TextView grandTotal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        totalAmount= (TextView) findViewById(total);
+        salesTax= (TextView) findViewById(R.id.sales_tax);
+        tip= (TextView) findViewById(R.id.tip);
+        grandTotal= (TextView) findViewById(R.id.grand_total);
+
+        Intent intent=getIntent();
+        int total=intent.getIntExtra("AMOUNT",0);
+        totalAmount.setText("Total: "+total);
+
+        Intent intent2=getIntent();
+        int sales=intent2.getIntExtra("TAX",0);
+        salesTax.setText("Sales Tax: "+sales);
+
+        Intent intent3=getIntent();
+        int tips=intent3.getIntExtra("TIPS",0);
+        tip.setText("Tip: "+ tips);
+
+        Intent intent4=getIntent();
+        int grand=intent4.getIntExtra("GRAND",0);
+        grandTotal.setText("Grand Total: "+ grand);
+    }
+}
